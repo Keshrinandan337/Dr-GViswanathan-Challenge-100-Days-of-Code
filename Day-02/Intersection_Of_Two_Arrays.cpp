f3932f586a67e0d147638e7df5c113b1c230c6b9
Keshri Nandan <142864787+Keshrinandan337@users.noreply.github.com>
2026-06-14 13:43:45 +0530

    Refactor intersection function for better clarity

--- Day-02/Intersection_Of_Two_Arrays.cpp
+++ Day-02/Intersection_Of_Two_Arrays.cpp
@@ -4,33 +4,31 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-       int i = 0 , j = 0;
-       vector<int> ans;
 
-       sort(nums1.begin(), nums1.end());
-       sort(nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
 
-       while(i < nums1.size() && j < nums2.size()){
+        vector<int> ans;
 
-        if (!ans.empty() && ans.back() == nums1[i]) {
-            i++;
-            continue;
+        int i = 0, j = 0;
+
+        while(i < nums1.size() && j < nums2.size()) {
+
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]) {
+                j++;
+            }
+            else {
+                if(ans.empty() || ans.back() != nums1[i]) {
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
         }
 
-
-        if ( nums1[i] == nums2[j]){
-            ans.push_back(nums1[i]);
-            i++;
-            j++;
-        }
-        else if ( nums1[i] < nums2[j]){
-            i++;
-        }
-        else {
-            j++;
-        }
-
-       }
-       return ans;
+        return ans;
     }
 };
