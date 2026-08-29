52c18023168ccbf12b5782112975de6992cdea5a
Keshri Nandan <142864787+Keshrinandan337@users.noreply.github.com>
2026-08-29 23:22:01 +0530

    Create Intersection of two array.cpp

---
+++ Day-15/Intersection of two array.cpp
@@ -0,0 +1,32 @@
+// leetcode 350
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        // sort these arrays
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        vector<int>result;
+
+        // using 2 pointer approch
+        int i = 0;
+        int j = 0;
+        while(i<n && j<m){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] == nums2[j]){
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return result;  
+    }
+};
