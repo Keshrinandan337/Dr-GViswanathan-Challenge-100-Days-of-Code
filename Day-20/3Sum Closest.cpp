fe7e199263c605b86bd5a979a289f555c143288a
Keshri Nandan <142864787+Keshrinandan337@users.noreply.github.com>
2026-08-29 23:29:43 +0530

    Implement threeSumClosest function for LeetCode 16

---
+++ Day-20/3Sum Closest.cpp
@@ -0,0 +1,31 @@
+// leetcode 16
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        int closest_sum = 100000;
+
+        sort(begin(nums),end(nums));
+
+        for(int k=0; k<=n-3; k++){
+            int i = k+1;
+            int j = n-1;
+            
+            while(i<j){
+                int sum = nums[k] + nums[i] + nums[j];
+
+                if(abs(target - sum) < abs(target  - closest_sum)){
+                    closest_sum = sum;
+                }
+
+                if(sum < target){
+                    i++;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+    return closest_sum;     
+    }
+};
