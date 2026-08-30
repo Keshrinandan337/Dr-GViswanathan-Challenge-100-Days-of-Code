55b177afcae25d4de249dab3faab8e7428c60d59
Keshri Nandan <142864787+Keshrinandan337@users.noreply.github.com>
2026-08-30 20:43:10 +0530

    Implement Kadane's Algorithm for max subarray sum

---
+++ Day-23/Maximum Subarray.cpp
@@ -0,0 +1,18 @@
+//leetcode 53
+// Kadane's Algorithm
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int totalSum = 0;
+        for(int i = 0;i<nums.size();i++){
+            totalSum += nums[i];
+            maxSum = max(totalSum,maxSum);
+            if(totalSum < 0){
+                totalSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
